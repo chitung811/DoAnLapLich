fdc6cb2675cfafbd6678047223a0f5c08ac43cec
chitung811 <chitung811@gmail.com>
2019-11-18 00:04:47 +0700

    Test

---
+++ Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace UngDungLapLich
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.DTPicker = new System.Windows.Forms.DateTimePicker();
+            this.Today = new System.Windows.Forms.Button();
+            this.btnThuHai = new System.Windows.Forms.Button();
+            this.btnThuBa = new System.Windows.Forms.Button();
+            this.btnThuTu = new System.Windows.Forms.Button();
+            this.btnThuNam = new System.Windows.Forms.Button();
+            this.btnThuSau = new System.Windows.Forms.Button();
+            this.btnThuBay = new System.Windows.Forms.Button();
+            this.btnChuNhat = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 480);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel5);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Location = new System.Drawing.Point(3, 32);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(770, 448);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.Today);
+            this.panel3.Controls.Add(this.DTPicker);
+            this.panel3.Location = new System.Drawing.Point(3, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(770, 26);
+            this.panel3.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button2);
+            this.panel4.Controls.Add(this.button1);
+            this.panel4.Controls.Add(this.btnChuNhat);
+            this.panel4.Controls.Add(this.btnThuBay);
+            this.panel4.Controls.Add(this.btnThuSau);
+            this.panel4.Controls.Add(this.btnThuNam);
+            this.panel4.Controls.Add(this.btnThuTu);
+            this.panel4.Controls.Add(this.btnThuBa);
+            this.panel4.Controls.Add(this.btnThuHai);
+            this.panel4.Location = new System.Drawing.Point(3, 3);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(764, 44);
+            this.panel4.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(110, 53);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(561, 392);
+            this.panel5.TabIndex = 1;
+            this.panel5.Paint += new System.Windows.Forms.PaintEventHandler(this.panel5_Paint);
+            // 
+            // DTPicker
+            // 
+            this.DTPicker.Location = new System.Drawing.Point(295, 3);
+            this.DTPicker.Name = "DTPicker";
+            this.DTPicker.Size = new System.Drawing.Size(198, 20);
+            this.DTPicker.TabIndex = 0;
+            // 
+            // Today
+            // 
+            this.Today.Location = new System.Drawing.Point(499, 0);
+            this.Today.Name = "Today";
+            this.Today.Size = new System.Drawing.Size(75, 23);
+            this.Today.TabIndex = 1;
+            this.Today.Text = "Hôm nay";
+            this.Today.UseVisualStyleBackColor = true;
+            this.Today.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnThuHai
+            // 
+            this.btnThuHai.Location = new System.Drawing.Point(107, 3);
+            this.btnThuHai.Name = "btnThuHai";
+            this.btnThuHai.Size = new System.Drawing.Size(75, 38);
+            this.btnThuHai.TabIndex = 2;
+            this.btnThuHai.Text = "Thứ hai";
+            this.btnThuHai.UseVisualStyleBackColor = true;
+            // 
+            // btnThuBa
+            // 
+            this.btnThuBa.Location = new System.Drawing.Point(188, 3);
+            this.btnThuBa.Name = "btnThuBa";
+            this.btnThuBa.Size = new System.Drawing.Size(75, 38);
+            this.btnThuBa.TabIndex = 3;
+            this.btnThuBa.Text = "Thứ ba";
+            this.btnThuBa.UseVisualStyleBackColor = true;
+            // 
+            // btnThuTu
+            // 
+            this.btnThuTu.Location = new System.Drawing.Point(269, 3);
+            this.btnThuTu.Name = "btnThuTu";
+            this.btnThuTu.Size = new System.Drawing.Size(75, 38);
+            this.btnThuTu.TabIndex = 4;
+            this.btnThuTu.Text = "Thứ tư";
+            this.btnThuTu.UseVisualStyleBackColor = true;
+            // 
+            // btnThuNam
+            // 
+            this.btnThuNam.Location = new System.Drawing.Point(350, 3);
+            this.btnThuNam.Name = "btnThuNam";
+            this.btnThuNam.Size = new System.Drawing.Size(75, 38);
+            this.btnThuNam.TabIndex = 5;
+            this.btnThuNam.Text = "Thứ năm";
+            this.btnThuNam.UseVisualStyleBackColor = true;
+            // 
+            // btnThuSau
+            // 
+            this.btnThuSau.Location = new System.Drawing.Point(431, 3);
+            this.btnThuSau.Name = "btnThuSau";
+            this.btnThuSau.Size = new System.Drawing.Size(75, 38);
+            this.btnThuSau.TabIndex = 6;
+            this.btnThuSau.Text = "Thứ sáu";
+            this.btnThuSau.UseVisualStyleBackColor = true;
+            // 
+            // btnThuBay
+            // 
+            this.btnThuBay.Location = new System.Drawing.Point(512, 3);
+            this.btnThuBay.Name = "btnThuBay";
+            this.btnThuBay.Size = new System.Drawing.Size(75, 38);
+            this.btnThuBay.TabIndex = 7;
+            this.btnThuBay.Text = "Thứ bảy";
+            this.btnThuBay.UseVisualStyleBackColor = true;
+            // 
+            // btnChuNhat
+            // 
+            this.btnChuNhat.Location = new System.Drawing.Point(593, 3);
+            this.btnChuNhat.Name = "btnChuNhat";
+            this.btnChuNhat.Size = new System.Drawing.Size(75, 38);
+            this.btnChuNhat.TabIndex = 8;
+            this.btnChuNhat.Text = "Chủ nhật";
+            this.btnChuNhat.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 38);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Thứ hai";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(674, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 38);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Thứ hai";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 504);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Calendar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button Today;
+        private System.Windows.Forms.DateTimePicker DTPicker;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnChuNhat;
+        private System.Windows.Forms.Button btnThuBay;
+        private System.Windows.Forms.Button btnThuSau;
+        private System.Windows.Forms.Button btnThuNam;
+        private System.Windows.Forms.Button btnThuTu;
+        private System.Windows.Forms.Button btnThuBa;
+        private System.Windows.Forms.Button btnThuHai;
+    }
+}
+
