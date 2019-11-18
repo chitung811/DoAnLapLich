9d4d27c2e845013a1a8a8a50c5eb28b51ddd12b3
Tamlu123 <buivantam2000@gmail.com>
2019-11-18 21:04:40 +0700

    code form1 bài 4

---
+++ tform1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lập_lịch_thông_báo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private PlanData job;
+
+        public PlanData Job
+        {
+            get { return job; }
+            set { job = value; }
+        }
+        
+    }
+    
+}
