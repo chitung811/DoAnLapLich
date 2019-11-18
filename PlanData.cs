baeff2eb6ffb9170059a95b5cc016ad65a68996f
Tamlu123 <buivantam2000@gmail.com>
2019-11-18 20:51:25 +0700

    bài 4 còn cái notify tự làm thêm code vào form 1

---
+++ PlanData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lập_lịch_thông_báo
+{
+    [Serializable]
+    public class PlanData
+    {
+        private List<PlanItem> job;
+
+        public List<PlanItem> Job
+        {
+            get { return job; }
+            set { job = value; }
+        }
+    }
+}
---
+++ PlanItem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lập_lịch_thông_báo
+{
+    public class PlanItem
+    {
+        private DateTime date;
+        private string job
+        { 
+            get { return job; }
+            set { job = value; }
+        }
+
+        private Point fromTime;
+
+        private Point FromTime
+        {
+            get { return formTime; }
+            set { fromTime = value;  }
+        }
+        private Point toTime;
+
+        private Point ToTime
+        {
+            get { return toTime; }
+            set { toTime = Value; }
+        }
+        private string status;
+
+        private string Status
+        {
+            get { return status; }
+            set { status = Value; }
+        }
+
+        public static List<string> ListStatus = new List<string>() { "DONE", "DOING", "COMING", "MISSED" };
+
+    }
+
+    public enum EplanItem
+    {
+        DONE,
+        DOING,
+        COMJING,
+        MISSED
+    }
+}
